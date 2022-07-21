44ca6a104f2a1a2280a99b95be26d8f26289595a
Morimx <82421851+Morimx@users.noreply.github.com>
2022-07-21 11:12:43 -0300

    added files and changes in docker compose

--- dbcategoria.js
+++ dbcategoria.js
@@ -4,9 +4,10 @@ const sql = require("mysql");
 function getCategorias() {
   return new Promise(function (resolve, reject) {
     let query_str = "SELECT * FROM TM_CATEGORIA";
+
     connection.query(query_str, function (err, rows) {
       if (err) {
-        return reject(err);
+      return reject(err);
       }
       resolve(rows);
     });
@@ -26,6 +27,21 @@ function getCategoriasxID(cat_id) {
     });
   });
 }
+
+
+function createCategoria(clase) {
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  //Insert a record in the "customers" table:
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
+}
+
 
 module.exports = {
   getCategorias: getCategorias,
